67768480ee2ab1a39ee4e2d456416cf44e04ed91
Max Kalashnikoff <geekmaks@gmail.com>
2024-01-12 13:57:39 +0100

    test: changes in the script and CI

--- .github/scripts/get_provider_tests.sh
+++ .github/scripts/get_provider_tests.sh
@@ -10,9 +10,9 @@ PROVIDERS_DIR="src/providers/"
 for file in $CHANGED_FILES; do
     # Check if the file is in the PROVIDERS_DIR
     if [[ $file == $PROVIDERS_DIR* ]]; then
-        # Extract the specific test name from the file path
-        PROVIDER_TEST_NAME=$(echo $file | sed "s|^$PROVIDERS_DIR||" | sed 's|/|::|g' | sed 's|\.rs$||')_provider
-        # Run specific test for that file
+        # Extract the specific provider name from the file path
+        PROVIDER_TEST_NAME=$(echo $file | sed "s|^$PROVIDERS_DIR||" | sed 's|/|::|g' | sed 's|\.rs$||')
+        # Print the provider name without new line for using as a list in the workflow
         echo -n "$PROVIDER_TEST_NAME "
     fi
 done
